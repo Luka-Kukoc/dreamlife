5cea8579cd7e6e38d234a1e3b9315bc54b14f68a
Luka Kukoc <kukoc.luka@gmail.com>
2022-01-13 16:39:40 +0100

    edit auth

--- src/Login.js
+++ src/Login.js
@@ -3,8 +3,10 @@ import Homepage from './pages/Home';
 import { useAuth0 } from "@auth0/auth0-react"
 import LoginButton from "./components/LoginButton"
 import Header from './components/Header';
+import { useNavigate } from "react-router-dom"
 function Login() {
   //test
+  const navigate = useNavigate()
   const { isAuthenticated, isLoading } = useAuth0()
   
   if(isLoading){
@@ -28,9 +30,7 @@ function Login() {
 
 
   return (
-    <div className="App">
-      <Homepage/>
-    </div>
+    navigate("/dreamlife/home")
   );
 }
 
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -5,7 +5,7 @@ import AddGoal from "../pages/AddGoals";
 import ActiveGoals from "../pages/ActiveGoals";
 import useLocalStorage from "../hooks/useLocalStorage";
 import EditGoal from "../components/EditGoal";
-
+import Homepage from "../pages/Home";
 
 const AppRouter = () => {
     const [goals, setGoals] = useLocalStorage("goals", [])
@@ -15,10 +15,11 @@ const AppRouter = () => {
         
             <Routes>
                 <Route path="/dreamlife" element={<Login />} />
-                <Route path="/goals" element={<Goals/>} />
-                <Route path="/addgoals" element={<AddGoal goals={goals} setGoals={setGoals}/>} />
-                <Route path="/activegoals" element={<ActiveGoals goals={goals} setGoals={setGoals}/>}/>
-                <Route path="/edit/:name" element={<EditGoal goals={goals} setGoals={setGoals}/>}/>
+                <Route path="/dreamlife/home" element={<Homepage/>}/>
+                <Route path="/dreamlife/goals" element={<Goals/>} />
+                <Route path="/dreamlife/addgoals" element={<AddGoal goals={goals} setGoals={setGoals}/>} />
+                <Route path="/dreamlife/activegoals" element={<ActiveGoals goals={goals} setGoals={setGoals}/>}/>
+                <Route path="/dreamlife/edit/:name" element={<EditGoal goals={goals} setGoals={setGoals}/>}/>
             </Routes>
         
         </BrowserRouter>
